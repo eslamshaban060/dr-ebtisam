ceca0406bbea5d7c6f7eddc0bd7c0b21defa787c
eslamshaban060 <eslamshaban060@gmail.com>
2025-09-12 20:42:16 +0200

    finshing login cicle in arabic version

--- drebtisame/src/app/page.js
+++ drebtisame/src/app/page.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 const page = () => {
   return (
-    <div>
-      <Link href="/login">الذهاب الى تسجيل الدخول</Link>
+    <div className=" bg-[var(--gr)] flex justify-center items-center w-[100vw] h-[100vh] ">
+      <Link className=" block !px-5 rounded-xl bg-white !py-3" href="/login">
+        الذهاب الى تسجيل الدخول
+      </Link>
     </div>
   );
 };
